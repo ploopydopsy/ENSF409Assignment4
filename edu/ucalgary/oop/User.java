5f91cc6330888c31367aef87e16d94baba541655
ploopydopsy <william.adebiyi@gmail.com>
2025-03-07 12:11:06 -0700

    user. java lay out

--- edu/ucalgary/oop/User.java
+++ edu/ucalgary/oop/User.java
@@ -7,7 +7,38 @@ public class User {
     private Subscription[] subscriptions = new Subscription[0];
     private Setting[] settings = new Setting[0];
 
-    // Your code here
+    // Constructors
+    public User(String username){
+        this.username = username;
+        this.subscriptions = new Subscription[0];
+    }
+    public User (String username, String category, String value) {
+        this.username = username;
+        this.subscriptions = new Subscription[0];
+    }
+
+    public User(String username, Subscription[] subscriptions) {
+        this.username = username;
+        this.subscriptions = subscriptions;
+    }
+
+    // getters
+
+    public Setting[] getSettings() {
+        return settings;
+    }
+
+    public String getCurrentSetting(String category) {
+
+    }
+
+    public Subscription[] getSubscriptions() {
+        return subscriptions;
+    }
+
+    public String getUsername() {
+        return username;
+    }
 
     private String getDefaultValueForCategory(String category) throws Exception {
         String[][] allCategories = new String[][] {
@@ -25,5 +56,41 @@ public class User {
         }
         return null;
     }
-}
 
+    // setters
+
+    public void setSettings(Setting[] settings) {
+        this.settings = settings;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    // subscription functions
+
+    public void addSubscription(Subscription subscription) {
+        subscriptions = Arrays.copyOf(subscriptions, subscriptions.length + 1);
+    }
+
+    public void removeSubscription(Subscription subscription) {
+        subscriptions = Arrays.copyOf(subscriptions, subscriptions.length - 1);
+    }
+
+    public void clearSubscriptions() {
+        subscriptions = new Subscription[0];
+    }
+
+
+
+    //misc
+    public String playContent (Content media) {
+        //TODO: FIX ME
+    }
+
+
+    public void removeSetting(String category) {
+
+    }
+
+}
